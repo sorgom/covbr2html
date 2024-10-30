5cfffc570db2be68863793c936e0f7282b989aa3
sorgom <110917257+sorgom@users.noreply.github.com>
2024-10-30 23:59:58 +0100

    main modified

--- code/main.cpp
+++ code/main.cpp
@@ -1,20 +1,26 @@
 #include <Covbr2Html.h>
 #include <glob.h>
+#include <iostream>
 
 INT32 ret = 0;
 
+void covFunc(const CONST_C_STRING item)
+{
+    if (not Covbr2Html::convert(item))
+    {
+        ret = 1;
+    }
+}
+
 int main(const INT32 argc, const CONST_C_STRING* const argv)
 {
-    globFunc func = [](const CONST_C_STRING item)
-    {
-        if (not Covbr2Html::convert(item))
-        {
-            ret = 1;
-        }
-    };
     for (INT32 i = 1; (i < argc) and (ret == 0); ++i)
     {
-        glob(argv[i], func);
+        glob(argv[i], &covFunc);
+    }
+    if (ret != 0)
+    {
+        std::cerr << "Error converting files" << '\n';
     }
     return ret;
 }
