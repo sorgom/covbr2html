f61b18e37177403913b224dee9a5826e2e0b2e04
sorgom <110917257+sorgom@users.noreply.github.com>
2024-11-02 13:52:15 +0100

    ready to merge

---
+++ py/sompy.py
@@ -0,0 +1,6 @@
+# This file is used to add sompy/somutil to the sys.path
+from sys import path as syspath
+from os.path import dirname, abspath, join
+sompy_path = abspath(join(dirname(__file__), '../sompy/somutil'))
+if sompy_path not in syspath:
+    syspath.insert(0, sompy_path)
