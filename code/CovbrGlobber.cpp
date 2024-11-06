58f8bf7bd5257a8c21408aef61a70dc9574b2ef3
sorgom <110917257+sorgom@users.noreply.github.com>
2024-11-06 15:10:12 +0100

    ok for dev

--- code/CovbrGlobber.cpp
+++ code/CovbrGlobber.cpp
@@ -2,36 +2,24 @@
 #include <Covbr2Html.h>
 #include <SOM/TraceMacros.h>
 #include <iostream>
-#include <iso646.h>
 
 void CovbrGlobber::process(const CONST_C_STRING item)
 {
-    TRACE_FUNC_TIME()
-    mThreads.push_back(std::thread(&CovbrGlobber::treadFunc, this, std::string(item)));
+    mThreads.push_back(std::thread(&CovbrGlobber::threadFunc, this, std::string(item)));
 }
 
 INT32 CovbrGlobber::ret()
 {
     TRACE_FUNC_TIME()
-    for (auto& th : mThreads)
-    {
-        th.join();
-    }
+    for (auto& th : mThreads) th.join();
     mThreads.clear();
-    if (_ret != 0)
-    {
-        std::cerr << _ret << " errors\n";
-    }
+    if (_ret != 0) std::cerr << _ret << " errors\n";
     return _ret;
 }
 
-void CovbrGlobber::treadFunc(const std::string&& file)
+void CovbrGlobber::threadFunc(const std::string&& file)
 {
     TRACE_FUNC_TIME()
-    TRACE_VAR(file)
-    if (not Covbr2Html::convert(file, mWb, mHc))
-    {
-        ++_ret;
-    }
+    if (not Covbr2Html::convert(file, mWb, mHc)) ++_ret;
 }
 
